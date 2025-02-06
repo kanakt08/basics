2996a52bd652520d492dadf47f11f85776035955
kanakt08 <tayal8621@gmail.com>
2025-02-06 23:28:24 +0530

    Add files via upload

---
+++ check_armstrong.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n,digit=0,m,r,result=0;
+    printf("enter the number : ");
+    scanf("%d",&n);
+    m=n;
+
+    while(n>0){
+        digit++;
+        n=n/10;
+    }
+    n=m;
+
+    while(n>0){
+        r=n%10;
+        result+=pow(r,digit);
+        n=n/10;
+    }
+
+    if(result==m){
+        printf("number is armstrong");
+    }
+    else{
+        printf("Number is not prime.");
+    }
+    return 0;
+}
